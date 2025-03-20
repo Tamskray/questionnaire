9f3b9e7f0899afb205a06b4bdf5ee43b390d5338
moja-utoma <ryabova.yelizaveta@gmail.com>
2025-03-20 12:40:48 +0200

    Changes for deploy

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const quizRoutes = require("./src/routes/quizRoutes");
-const completionRoutes = require("./src/routes/completionRoutes");
+const quizRoutes = require("../src/routes/quizRoutes");
+const completionRoutes = require("../src/routes/completionRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 app.use("/api/quizzes", quizRoutes);
 app.use("/api/completions", completionRoutes);
